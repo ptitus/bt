180344ede960c7e85fa18672bd385afe18e7c972
ptitus <peter@titus-heilbronn.de>
2020-06-13 22:05:13 +0200

    SSH Key erstell, Login erfolgt aber weiter mit Passwort

--- vagrant/ubuntu-18-04_mate-desktop/base.sh
+++ vagrant/ubuntu-18-04_mate-desktop/base.sh
@@ -7,3 +7,4 @@ adduser user --gecos "My User,,," --disabled-password
 echo "user:linux" | chpasswd
 echo "user ALL=(ALL) ALL" >> /etc/sudoers
 usermod -a -G vboxsf user
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBxBuN+4taQ5IbKJXr3oSdVny4/769YSqxBrjTv5roQerBfDeCgv6rIoSfKDXB4sbGSnQCRSUMxc/qJPm86SJvlBOjr0YcrxC+VfRAcZNu08SfKtzhsQIGknxrkMA8j2bFzaZbYm4xPn/LfBfC/TAx82tM0tK4R/36IZ4Pkppa7poZKumai/W+1iXZvlDCqd44LdErBOXEuPMkKxYXIV3h1uAeV71krngqIenmQ+zauTxbjzPGZtg5w6XxeHureJ5JfbM4v04JDvSIFSUt/t1S9WdDJCzsvYZji7aVotj4e1rlGCb0gjDC0XMflhqAzak97S2/8ZPNiv8hBjXjioyj Key zur Automation per ssh" > /home/user/.ssh/authorized_keys
